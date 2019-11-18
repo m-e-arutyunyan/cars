b1f30c9b6b7c1666e5f1f4fcaee34de805abe83a
marutyunyan <marutyunyan@gpm.ru>
2019-11-18 13:20:30 +0300

    еее

--- cars.js
+++ cars.js
@@ -1,19 +1,22 @@
-        var app = new Vue({
-				el: '#app',
-				data: {
-					message: 'Road2Vue!',
-					car3left: 300,
-					car2left: 200
-				}
-			})
-		
-		function tick() {
-			  app.car3left = app.car3left + 1;	
-		}
+//const Vue = require('../node_modules/vue/dist/vue');
+//const Vue = require('vue');
 
-		var timerId = setInterval(tick, 50);
+var app = new Vue({
+  el: "#app",
+  data: {
+    message: "Road2Vue!",
+    car3left: 300,
+    car2left: 200
+  }
+});
 
-		// через 5 сек остановить повторы
-		setTimeout(function () {
-				clearInterval(timerId);
-			}, 5000);	
+function tick() {
+  app.car3left = app.car3left + 1;
+}
+
+var timerId = setInterval(tick, 50);
+
+// через 5 сек остановить повторы
+setTimeout(function() {
+  clearInterval(timerId);
+}, 5000);
